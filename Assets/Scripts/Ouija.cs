79c5c1978057fd94a2582445cc07be762e730bff
Conner <conner.sommerfield@gmail.com>
2023-11-15 00:15:55 -0800

    Add Ouija Text

--- Assets/Scripts/Ouija.cs
+++ Assets/Scripts/Ouija.cs
@@ -1,29 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
-
+using TMPro;
 public class Ouija : MonoBehaviour
 {
+    public string answer = "";
+    public GameObject textObject;
+    private TextMeshPro text;
     // Start is called before the first frame update
     void Start()
     {
+        text = textObject.GetComponent<TextMeshPro>();
         foreach (Transform child in transform)
         {
             BoxCollider collider = child.GetComponent<BoxCollider>();
-            collider.gameObject.AddComponent<LetterClickHandler>();
+            LetterClickHandler clickHandler = collider.gameObject.AddComponent<LetterClickHandler>();
+            clickHandler.SetOuija(this);
         }
+    }
+
+    private void Update()
+    {
+        text.text = answer;
     }
 }
 
 public class LetterClickHandler : MonoBehaviour
 {
+    private Ouija ouija;
+
+    public void SetOuija(Ouija ouija)
+    {
+        this.ouija = ouija;
+    }
+
     void OnMouseDown()
     {
-        // Get the letter associated with this collider
-        char letter = transform.name[0];
+        string keyName = transform.name;
 
-        // Do something with the letter, for example, print it
-        Debug.Log("Clicked on letter: " + letter);
+        if (keyName == "Yes")
+        {
+            SubmitAnswer(ouija.answer);
+        }
+        else if (keyName == "No")
+        {
+            ouija.answer = "";
+            Debug.Log("Cleared Answer");
+        }
+        else
+        {
+            Debug.Log("Clicked On: " + keyName);
+            ouija.answer = string.Concat(ouija.answer, keyName);
+        }
+    }
+
+    public void SubmitAnswer(string word)
+    {
+        Debug.Log("Word is: " + word);
     }
 }
