9049c6669f7e3f81ee81a638bfd8fcc0cf3664af
Conner <conner.sommerfield@gmail.com>
2023-11-14 20:56:44 -0800

    Improve Colliders

--- Assets/Scripts/AuxilaryView.cs
+++ Assets/Scripts/AuxilaryView.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChairView : MonoBehaviour
+public class AuxilaryView : MonoBehaviour
 {
     public Camera mainCamera;
-    public Camera pianoCamera;
+    public Camera auxilaryCamera;
 
     private void Start()
     {
-        pianoCamera.enabled = false;
+        auxilaryCamera.enabled = false;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && pianoCamera.enabled)
+        if (Input.GetKeyDown(KeyCode.Escape) && auxilaryCamera.enabled)
         {
             togglePianoView();
         }
@@ -28,7 +28,7 @@ public class ChairView : MonoBehaviour
     private void togglePianoView()
     {
         mainCamera.enabled = !mainCamera.enabled;
-        pianoCamera.enabled = !pianoCamera.enabled;
+        auxilaryCamera.enabled = !auxilaryCamera.enabled;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
     }
